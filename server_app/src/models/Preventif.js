ba1eb6d4cbecbdc536883fe4169fbe995d6a1b12
hichamsbihi <sbihi.hicham96@gmail.com>
2021-05-17 23:51:54 +0200

    creating project models

---
+++ server_app/src/models/Preventif.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const PreventifSchema = new mongoose.Schema({
+  ots: {
+    type: Number,
+  },
+
+  id_equipement: [],
+
+  commentaire: {
+    type: String,
+  },
+
+  date: {
+    type: Date,
+  },
+});
+
+const Preventif = new mongoose.model("Preventif", PreventifSchema);
+exports.Preventif = Preventif;
