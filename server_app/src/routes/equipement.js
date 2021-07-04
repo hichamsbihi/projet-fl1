9d338fb0ec24f2645c1bd77c79ee2fcc745ba638
hichamsbihi <sbihi.hicham96@gmail.com>
2021-07-04 23:58:25 +0200

    test

--- server_app/src/routes/equipement.js
+++ server_app/src/routes/equipement.js
@@ -3,9 +3,13 @@ import { Router } from "express";
 
 import { setHeaders } from "./middlewares.js";
 import { ERROR_MESSAGES_EN, SUCCESS_MESSAGES_EN } from "../core/constants.js";
-import {EQUIPEMENT,CORRECTIF,PREVENTIF,STOCK} from "../models/Equipement.js";
+import {
+  EQUIPEMENT,
+  CORRECTIF,
+  PREVENTIF,
+  STOCK,
+} from "../models/equipement.js";
 // import {equipementSerializer} from "../serializers/equipement_serializer.js";
-
 
 const router = Router();
 const _JSON2STR = JSON.stringify;
@@ -274,48 +278,41 @@ router.get("/api/v1.0/getEquipement", async (req, res) => {
 });
 
 /*
-* @api {get} /api/v1.0/equipement/stock Get stock
-* @apiVersion 1.0.0
-* @apiGroup Stock
-* @apiHeader {String} x_access_token Users unique jwt-access-key.
-* @apiSuccess {Object} Stock all stock data from all tables.
-* @apiSuccessExample {json} Success, the Order state has been updated to pickedUp
-*    HTTP/1.1 200 OK
-*    {
-*      
-*    }
-* @apiErrorExample {json} stock not found
-*    HTTP/1.1 404 Error has occurred, the response describe the error
-*    {
-*      "demande_state":"stock data not found !!",
-*      "err_number" : 9
-*      }
-*/
+ * @api {get} /api/v1.0/equipement/stock Get stock
+ * @apiVersion 1.0.0
+ * @apiGroup Stock
+ * @apiHeader {String} x_access_token Users unique jwt-access-key.
+ * @apiSuccess {Object} Stock all stock data from all tables.
+ * @apiSuccessExample {json} Success, the Order state has been updated to pickedUp
+ *    HTTP/1.1 200 OK
+ *    {
+ *
+ *    }
+ * @apiErrorExample {json} stock not found
+ *    HTTP/1.1 404 Error has occurred, the response describe the error
+ *    {
+ *      "demande_state":"stock data not found !!",
+ *      "err_number" : 9
+ *      }
+ */
 router.get("/api/v1.0/equipement/stock", (req, res) => {
-
- try {
-
-     STOCK.find({}, (err, reply) => {
-       if (err || !reply) {
-         setHeaders({ res, status: 404 }).then(() =>
-           res.end(
-             _JSON2STR({ err_number: 9, demande_state: ERROR_MESSAGES_EN[9] })
-           )
-         );
-       } else {
-
-         setHeaders({ res, status: 200 }).then(() =>
-           res.end(_JSON2STR(reply))
-         );
-       }
-     });
-   
- } catch (e) {
-
-   console.log(e);
-   setHeaders({ res, status: 450 }).then(() =>
-     res.end(_JSON2STR({ err_number: 1, demande_state: ERROR_MESSAGES_EN[1] }))
-   );
- }
+  try {
+    STOCK.find({}, (err, reply) => {
+      if (err || !reply) {
+        setHeaders({ res, status: 404 }).then(() =>
+          res.end(
+            _JSON2STR({ err_number: 9, demande_state: ERROR_MESSAGES_EN[9] })
+          )
+        );
+      } else {
+        setHeaders({ res, status: 200 }).then(() => res.end(_JSON2STR(reply)));
+      }
+    });
+  } catch (e) {
+    console.log(e);
+    setHeaders({ res, status: 450 }).then(() =>
+      res.end(_JSON2STR({ err_number: 1, demande_state: ERROR_MESSAGES_EN[1] }))
+    );
+  }
 });
 export const equipement_Router = router;
