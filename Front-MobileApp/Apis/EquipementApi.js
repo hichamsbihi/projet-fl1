4531d6ae0977942e3ea25bc64a257036b2e06b84
accchraf <ac.boufdil@gmail.com>
2021-07-07 22:27:37 +0200

    commit from achrafhost

--- Front-MobileApp/Apis/EquipementApi.js
+++ Front-MobileApp/Apis/EquipementApi.js
@@ -2,9 +2,9 @@ import React from "react";
 import axios from "axios";
 
 const EquipementApi = (id) =>
-  axios.get("http://10.130.227.186:8089/api/v1.0/getequipement/" + id);
+  axios.get("http://192.168.0.18:8089/api/v1.0/getequipement/" + id);
 
 const StockApi = () =>
-  axios.get("http://10.130.227.186:8089/api/v1.0/equipement/stock");
+  axios.get("http://192.168.0.18:8089/api/v1.0/equipement/stock");
 
 export default { EquipementApi, StockApi };
